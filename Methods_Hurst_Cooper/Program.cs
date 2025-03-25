354e02d960f3800367e58999cf3075fddadf2023
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-03-25 08:58:40 -0500

    This is a method

--- Methods_Hurst_Cooper/Program.cs
+++ Methods_Hurst_Cooper/Program.cs
@@ -6,14 +6,5 @@ Console.WriteLine(Number1 * Number2);
 
 Console.WriteLine("Enter Your Name");
 string Name = Console.ReadLine();
-Console.WriteLine($"Hello {Name}! It is nice to meet you! How was it to live with the Dinosaurs?");
-Console.WriteLine("Answer the Question with Good or Bad");
-string Answer = Console.ReadLine();
-if (Answer == "Good")
-{
-    Console.WriteLine("Thats Good to Hear! >:)");
-}
-else if (Answer == "Bad")
-{
-    Console.WriteLine("Don't Lie to Me!");
-}
+Console.WriteLine($"Hello {Name}! It is nice to meet you!");
+Console.WriteLine("Have a fantastic day");
