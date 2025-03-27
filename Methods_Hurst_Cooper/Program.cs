de2af12cc9a4af9ee95a78f8dd77ca85b7f78dc0
Ragnarok1026 <cooperhurst1026@gmail.com>
2025-03-27 10:21:01 -0500

    hrigweh
    jejrthbet

--- Methods_Hurst_Cooper/Program.cs
+++ Methods_Hurst_Cooper/Program.cs
@@ -1,10 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Type 2 Numbers");
-int Number1 = int.Parse(Console.ReadLine());
-int Number2 = int.Parse(Console.ReadLine());
-Console.WriteLine(Number1 * Number2);
 
+//This Calls the Methods
+{
+    Numbers();
+    EnterName();
+}
+
+//This asks for 2 numbers and multiplies them together
+static void Numbers()
+{
+    Console.WriteLine("Type 2 Numbers");
+    int Number1 = int.Parse(Console.ReadLine());
+    int Number2 = int.Parse(Console.ReadLine());
+    Console.WriteLine(Number1 * Number2);
+}
+
+//this asks for your name and prints the statment with your name
+static void EnterName()
+{
 Console.WriteLine("Enter Your Name");
 string Name = Console.ReadLine();
 Console.WriteLine($"Hello {Name}! It is nice to meet you!");
 Console.WriteLine("Have a fantastic day");
+}
+
+
+
